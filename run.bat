fb8ae6d50745704b65b3c43de81f78a10860148b
Weipeng Zhang <javawebzwp@gmail.com>
2014-12-12 12:12:26 +0800

    update paper

---
+++ run.bat
@@ -0,0 +1,10 @@
+del pose.aux
+del pose.lof
+del pose.log
+del pose.lot
+del pose.out
+del pose.toc
+
+del "C:\CTEX\UserData\fonts\tfm\public\rsfs\rsfs5.tfm"
+del "C:\CTEX\UserData\fonts\tfm\public\rsfs\rsfs10.tfm"
+xelatex pose.tex
